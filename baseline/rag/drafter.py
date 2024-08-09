d25f98be76cf6436c01a61f74a54ab9c42b0d428
Emelyan Egorov <113930312+Pomelkin@users.noreply.github.com>
2024-08-10 02:50:48 +0300

    Add help func for calculating centroids, getting particular points from cluster

--- baseline/rag/drafter.py
+++ baseline/rag/drafter.py
@@ -1,5 +1,8 @@
+from sklearn.cluster import KMeans
 from baseline.knowledge_base import QdrantKnowledgeBase
 import numpy as np
+from .utils import choose_instances_from_clusters, calculate_centroids
+from qdrant_client import models
 
 
 class Drafter:
@@ -11,3 +14,10 @@ class Drafter:
         retrieved_points, embedding = self._knowledge_base.get_similar_points(query)
         retrieved_vectors = np.array([point.vector for point in retrieved_points])
         return retrieved_points, embedding
+
+    @staticmethod
+    def estimate_points(points: list[models.ScoredPoint]):
+        vectors = np.array([point.vector for point in points])
+        clusters = KMeans(n_clusters=3).fit_predict(vectors)
+        ind = choose_instances_from_clusters(clusters)
+        centroids = calculate_centroids(clusters, vectors)
--- baseline/rag/utils.py
+++ baseline/rag/utils.py
@@ -2,10 +2,26 @@ import numpy as np
 
 
 def choose_instances_from_clusters(clusters: np.ndarray) -> list[int]:
-    """Randomly choose one instance from each cluster"""
-    num_clusters = np.unique(clusters)
+    """Select instances from clusters based on cosine similarity and randomness.
+    The function works as follows:
+    1. Select the top-1 vector from 9 clusters with the closest cosine distance to the input question.
+    2. From each of the remaining 2 clusters, select a random vector."""
+
+    top_1_cluster = clusters[0]
+    num_clusters = np.unique(clusters[clusters != top_1_cluster])
+
+    # select random vectors from each cluster
     indexes = [
         np.random.choice(np.where(clusters == cluster_ind)[0])
         for cluster_ind in num_clusters
     ]
+    indexes.insert(0, 0)  # insert top-1 vector (0)
     return indexes
+
+
+def calculate_centroids(clusters: np.ndarray, vectors: np.ndarray) -> np.ndarray:
+    """Calculate the centroids of each cluster."""
+    centroids = np.zeros((len(np.unique(clusters)), vectors.shape[1]))
+    for cluster_ind in np.unique(clusters):
+        centroids[cluster_ind] = vectors[cluster_ind == clusters].mean(axis=0)
+    return centroids
